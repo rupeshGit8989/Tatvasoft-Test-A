715e45b7b7546b782d1773375c6e3b31497c4a96
ankitshantiinfotech <ankit.shantiinfotech@gmail.com>
2022-01-20 07:50:20 +0530

    First commit

---
+++ app/src/main/java/com/tatvasoft/test/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tatvasoft.test.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tatvasoft.test.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
